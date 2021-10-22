c54097c91184c5f17751cf65dff1a7da85a5b12c
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-22 14:15:40 -0300

    ProductService class and PrsuctResource Class update

--- src/main/java/com/marcossa/course/services/ProductService.java
+++ src/main/java/com/marcossa/course/services/ProductService.java
@@ -3,11 +3,17 @@ package com.marcossa.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.marcossa.course.entities.Product;
 import com.marcossa.course.repositories.ProductRepository;
+import com.marcossa.course.services.exceptions.DatabaseException;
+import com.marcossa.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -22,6 +28,38 @@ public class ProductService {
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Product update(Long id, Product obj) {
+		try {
+			Product entity = repository.getOne(id);
+			UpdateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void UpdateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+		
 	}
 
 }
